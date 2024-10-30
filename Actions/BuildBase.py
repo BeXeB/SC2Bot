f69874deb710a1d8859ac9676a6ff3ccef50082e
selectionator <tlaust18@student.aau.dk>
2024-10-30 14:04:31 +0100

    edit

--- Actions/BuildBase.py
+++ Actions/BuildBase.py
@@ -1,9 +1,10 @@
 from sc2.bot_ai import BotAI
 from sc2.ids.unit_typeid import UnitTypeId
+
 # Base = Command center
 # Bases should be in proximity of minerals, but no more than 1 for each cluster of minerals
 # This is what we should do:
-# 1. Search the map for clusters of minerals (Around 10? from the base)
+# 1. Search the map for clusters of minerals
 #   Find all minerals and keep them in a set
 #   Create a subset from the closest mineral and the minerals in a radius of like 10 of the mineral
 # 2. Implicitly "select" the optimal one (Either closest one to current units, or current base?)
@@ -13,12 +14,16 @@ from sc2.ids.unit_typeid import UnitTypeId
 # 4. The parametrized worker should then move to that spot and build a base there.
 #   Can possibly simply use the build method to move there and build
 
-# 1.
-# Find all minerals and keep them in a set
-
 class BaseBuilder():
-    #def FindOptimalNewBase(self, botai):
-        #print(UnitTypeId.COMMANDCENTER)
+    def FindOptimalNewBase(self, botai):
+        print("deez")
+        # Brug remaining fra CalculateAllCurrent... til at finde tætteste cluster til næste base
+        # Udregn det optimale center i midten af clusteret
+        # Byg COMMANDCENTER
+
+
+        # Måske hav lokal variabel til oldCurrentMinerals, og kald disse explicit i testbot, samt herover og -under.
+
 
     # Only for base rn, but change it to iterate through all the bases (IMPORTANT TO KEEP UP WITH ITERATIONS)
     # Should also be changed to select the minerals in a radius, rather than from base. This method is highly specific
@@ -37,6 +42,7 @@ class BaseBuilder():
             if mineral not in currentMinerals or oldCurrentMinerals:
                 remainingMinerals.append(mineral)
 
+
         return currentMinerals, remainingMinerals
 
     def TestCalculations(self, current, remaining):
@@ -49,3 +55,5 @@ class BaseBuilder():
         for mineral in remaining:
             print(mineral)
 
+# Todo:
+#
--- testbot.py
+++ testbot.py
@@ -4,20 +4,21 @@ from Actions.BuildBase import BaseBuilder
 class MyBot(BotAI):
     async def on_step(self, iteration: int):
         print(f"iteration: {iteration}")
-
+        cur = []
+        rem = []
         #rndwrk = self.workers.random
         #closestMineral = self.state.mineral_field.closest_to(rndwrk)
         #if not rndwrk.is_gathering:
         #    await self.do(rndwrk.gather(closestMineral))
         BB = BaseBuilder()
-        cur, rem = BB.CalculateAllCurrentAndRemainingMinerals(self, [])
+        if iteration == []:
+            cur, rem = BB.CalculateAllCurrentAndRemainingMinerals(self, [])
+        else:
+            cur, rem = BB.CalculateAllCurrentAndRemainingMinerals(self, cur)
 
         BB.TestCalculations(cur, rem)
         #cur, rem = BB.CalculateAllCurrentAndRemainingMinerals(self)
         #BB.TestCalculations(cur, rem)
         #BB.FindOptimalNewBase(self)
-
-
-
 
 
