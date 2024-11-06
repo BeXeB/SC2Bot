60972405ca217ab4addfe8dd233ce53e1053aa33
mkju19 <mkju19@student.aau.dk>
2024-11-06 11:26:53 +0100

    Update Sc2Constants.h

--- cpp_modules/src/sc2_state/Sc2Constants.h
+++ cpp_modules/src/sc2_state/Sc2Constants.h
@@ -4,8 +4,8 @@
 
 #ifndef SC2CONSTANTS_H
 #define SC2CONSTANTS_H
-static const int WORKER_PER_GEYSER = 3;
-static const int WORKER_PER_MINERAL_FIELD = 2;
-static const int MINERAL_PER_WORKER = 1;
-static const int VESPENE_PER_WORKER = 1;
+static constexpr int WORKER_PER_GEYSER = 3;
+static constexpr int WORKER_PER_MINERAL_FIELD = 2;
+static constexpr int MINERAL_PER_WORKER = 1;
+static constexpr int VESPENE_PER_WORKER = 1;
 #endif //SC2CONSTANTS_H
