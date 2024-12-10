d19eab537bf42d536379a88490af531b5ea1c6ef
Bence Imre Kiss <kissbenceimre99@gmail.com>
2024-12-10 13:53:50 +0100

    move base and contruction into their own functions

--- state_translator.py
+++ state_translator.py
@@ -7,6 +7,8 @@ from sc2.ids.ability_id import AbilityId
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.unit import Unit
 
+from testbot import MyBot
+
 
 @dataclass
 class Base:
@@ -37,7 +39,7 @@ class State:
     # workers that are currently busy, and the time left
     busy_workers: list[int]
 
-def translate_state(bot: 'MyBot') -> State:
+def get_bases(bot: 'MyBot') -> list[Base]:
     bases = []
     for townhall in bot.townhalls:
         vespene_collectors = bot.structures.filter(lambda s: s.type_id == UnitTypeId.REFINERY).closer_than(10, townhall)
@@ -48,6 +50,9 @@ def translate_state(bot: 'MyBot') -> State:
             incoming_vespene_collectors=len(vespene_collectors.filter(lambda s: s.build_progress < 1))
         )
         bases.append(base)
+    return bases
+
+def get_constructions(bot: 'MyBot') -> list[Construction]:
     # TODO: This is a very hacky way to get the pending constructions
     constructions = []
     for i in range(bot.worker_en_route_to_build(UnitTypeId.SUPPLYDEPOT)):
@@ -79,10 +84,15 @@ def translate_state(bot: 'MyBot') -> State:
     for th in bot.townhalls:
         if th.orders and th.orders[0].ability.id == AbilityId.COMMANDCENTERTRAIN_SCV:
             construction = Construction(
-                time_left= (1-th.orders[0].progress) * bot.WORKER_BUILD_TIME_SECONDS,
+                time_left=(1 - th.orders[0].progress) * bot.WORKER_BUILD_TIME_SECONDS,
                 unit_type=UnitTypeId.SCV
             )
             constructions.append(construction)
+    return constructions
+
+def translate_state(bot: 'MyBot') -> State:
+    bases = get_bases(bot)
+    constructions = get_constructions(bot)
     state = State(
         minerals=bot.minerals,
         vespene=bot.vespene,
@@ -91,6 +101,6 @@ def translate_state(bot: 'MyBot') -> State:
         population_limit=math.floor(bot.supply_cap),
         bases=bases,
         constructions=constructions,
-        busy_workers=[time for time in bot.busy_workers.values()]
+        busy_workers=[math.ceil(time) for time in bot.busy_workers.values()]
     )
     return state
