47fbf59aff2f08f4914bc407280b89fd25c6da78
mkju19 <mkju19@student.aau.dk>
2024-12-12 11:40:39 +0100

    added mcts_runner

---
+++ mcts_runner.py
@@ -0,0 +1,40 @@
+﻿import math
+import multiprocessing as mp
+
+from sc2_mcts import *
+class MctsRunner:
+    def __init__(self):
+        self.running : bool = False
+
+        self.next_action = Action.none
+        self.mcts = Mcts(state=State(),
+                         seed=0,
+                         rollout_depth=100,
+                         exploration=math.sqrt(2),
+                         value_heuristic=ValueHeuristic.UCT,
+                         rollout_heuristic=RolloutHeuristic.weighted_choice)
+        self.mcts_lock = mp.Lock()
+        self.mcts_thread: mp.Process = mp.Process(target=self.mcts_search_thread)
+
+
+    def mcts_search_thread(self):
+        while self.running:
+            with self.mcts_lock:
+                self.mcts.search(500)
+                self.next_action = self.mcts.get_best_action()
+
+    def start(self):
+        self.running = True
+        self.mcts_thread.start()
+
+    def update_state(self, state: State):
+        with self.mcts_lock:
+            self.mcts.update_root_state(state)
+
+    def get_next_action(self):
+            return self.next_action
+
+    def do_action(self, new_state: State):
+        with self.mcts_lock:
+            self.next_action = Action.none
+            self.mcts.update_root_state(new_state)
--- testbot.py
+++ testbot.py
@@ -1,9 +1,8 @@
 import math
-import threading
 
 from sc2.position import Point2
 
-from sc2_mcts import *
+from sc2_mcts import Action
 
 from sc2.bot_ai import BotAI
 from sc2.ids.unit_typeid import UnitTypeId
@@ -11,6 +10,7 @@ from sc2.ids.ability_id import AbilityId
 from sc2.unit import Unit
 
 from Actions.build_worker import WorkerBuilder
+from mcts_runner import MctsRunner
 from state_translator import translate_state
 
 from Actions.BuildBase import BaseBuilder
@@ -22,13 +22,14 @@ from worker_manager import WorkerManager, TownhallData, GasBuildingData, WorkerR
 STEPS_PER_SECOND = 22.4
 
 class MyBot(BotAI):
-    next_action: Action = Action.none
     # Initializing our bot with the BaseBuilder, such that we don't initialize a new BaseBuilder instance on every step.
     # Furthermore, we initialize the vespene_builder
     # Completed_bases is used to keep track of processed vespene extractors
     def __init__(self) -> None:
         self.completed_bases = set()
         self.busy_workers: dict[int, float] = {}
+        self.mcts = MctsRunner()
+        self.next_action = Action.none
 
     async def on_start(self):
         # self.CC_BUILD_TIME_STEPS: int = self.game_data.units[UnitTypeId.COMMANDCENTER.value]._proto.build_time
@@ -50,21 +51,7 @@ class MyBot(BotAI):
         self.supply_builder = SupplyBuilder(self)
         self.worker_manager = WorkerManager(self)
         self.worker_builder = WorkerBuilder(self)
-        self.mcts = Mcts(State(), 0, 100, math.sqrt(2), ValueHeuristic.UCT, RolloutHeuristic.weighted_choice)
-        self.mcts_thread = threading.Thread(target=self.mcts_search_thread)
-        self.mcts_thread.start()
-        self.next_action_mutex = threading.Lock()
-
-    def mcts_search_thread(self):
-        while True:
-            self.mcts.search(100)
-            while not self.next_action == Action.none:
-                self.mcts.search(500)
-                print("Search")
-            with self.next_action_mutex:
-                self.next_action = self.mcts.get_best_action()
-                print(f"Best action: {self.next_action}")
-            self.mcts.update_root_state(translate_state(self))
+        self.mcts.start()
 
 
     async def on_step(self, iteration: int) -> None:
@@ -79,13 +66,13 @@ class MyBot(BotAI):
         self.update_busy_workers()
         self.manage_workers()
 
-        match self.next_action:
+        match self.mcts.get_next_action():
             case Action.build_base:
                 if not self.can_afford(UnitTypeId.COMMANDCENTER):
                     return
                 new_base_location = await self.base_builder.find_next_base_location()
                 self.build_base(new_base_location)
-                # self.set_next_action()
+                self.mcts.do_action(translate_state(self))
             case Action.build_vespene_collector:
                 if not self.can_afford(UnitTypeId.REFINERY):
                     return
@@ -95,17 +82,20 @@ class MyBot(BotAI):
                     await self.vespene_builder.build_vespene_extractor(self.townhalls.random.position)
                     # TODO only add to completed if all the vespene extractors are built
                     self.completed_bases.add(th.tag)
+                    self.mcts.do_action(translate_state(self))
                 # self.set_next_action()
             case Action.build_worker:
                 if not self.can_afford(UnitTypeId.SCV):
                     return
                 # TODO if we are unable to build an scv, wait
                 await self.worker_builder.build_worker()
+                self.mcts.do_action(translate_state(self))
                 # self.set_next_action()
             case Action.build_house:
                 if not self.can_afford(UnitTypeId.SUPPLYDEPOT):
                     return
                 await self.supply_builder.build_supply()
+                self.mcts.do_action(translate_state(self))
                 # self.set_next_action()
 
     def set_next_action(self, action: Action = Action.none):
