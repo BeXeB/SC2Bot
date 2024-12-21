ca1c1485a612732d440100b12d6d39e8bd8fe418
mkju19 <mkju19@student.aau.dk>
2024-12-21 11:45:31 +0100

    Update Construction.cpp

--- cpp_modules/src/sc2_state/Construction.cpp
+++ cpp_modules/src/sc2_state/Construction.cpp
@@ -7,7 +7,6 @@
 //
 Sc2::Construction::Construction(const int constructionTime, const Action action) {
     _timeLeft = constructionTime;
-    _state = nullptr;
     _action = action;
 
     switch (action) {
