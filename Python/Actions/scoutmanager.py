be7ca4a96f90366103cec604e5ad0e45c9d77a0b
Selectionator <tobiaslausten@hotmail.com>
2025-03-24 13:19:20 +0100

    Changed manage_scouts' return types, access modifiers and location in the file

--- Python/Actions/scoutmanager.py
+++ Python/Actions/scoutmanager.py
@@ -16,16 +16,28 @@ class ScoutManager:
         self.target: Point2 = self.cluster_points[0]
         self.scout: Unit | None = None
         self.KITING_RANGE = 5
+        
+    def manage_scouts(self) -> None:
+        scout_list = self.bot.information_manager.get_workers(WorkerRole.SCOUT)
+        if not scout_list:
+            self.scout = self.bot.worker_manager.select_worker(self.target, WorkerRole.SCOUT)
+            if self.scout is not None:
+                self.bot.worker_manager.assign_worker(self.scout.tag, WorkerRole.SCOUT, None)
+            self.__sort_expansion_distances()
+            self.target = next(self.cluster_iter)
+        if self.scout is not None:
+            self.scout = self.bot.workers.by_tag(self.scout.tag)
+            self.__kite_scout(self.scout)
 
-    def kite_scout(self, scout: Unit) -> None:
+    def __kite_scout(self, scout: Unit) -> None:
         enemy_units = self.bot.enemy_units
-        if self.enemies_in_range(enemy_units, self.scout):
-            self.update_target()
+        if self.__enemies_in_range(enemy_units, self.scout):
+            self.__update_target()
         if scout.distance_to(self.target) < 5:
-            self.update_target()
+            self.__update_target()
         scout.move(self.target)
 
-    def update_target(self):
+    def __update_target(self):
         initial_position = self.target
         while True:
             self.target = next(self.cluster_iter)
@@ -35,29 +47,16 @@ class ScoutManager:
                 break
 
 
-    def enemies_in_range(self, enemies: Units, scout: Unit) -> bool:
+    def __enemies_in_range(self, enemies: Units, scout: Unit) -> bool:
         for enemy in enemies:
             if enemy.distance_to(scout) < enemy.ground_range + self.KITING_RANGE:
                 return True
         return False
 
+    def __sort_expansion_distances(self) -> None:
+        self.cluster_points.sort(reverse=True, key=self.__sorting_helper)
 
-    def manage_scouts(self):
-        scout_list = self.bot.information_manager.get_workers(WorkerRole.SCOUT)
-        if not scout_list:
-            self.scout = self.bot.worker_manager.select_worker(self.target, WorkerRole.SCOUT)
-            if self.scout is not None:
-                self.bot.worker_manager.assign_worker(self.scout.tag, WorkerRole.SCOUT, None)
-            self.sort_expansion_distances()
-            self.target = next(self.cluster_iter)
-        if self.scout is not None:
-            self.scout = self.bot.workers.by_tag(self.scout.tag)
-            self.kite_scout(self.scout)
-
-    def sort_expansion_distances(self):
-        self.cluster_points.sort(reverse=True, key=self.sorting_helper)
-
-    def sorting_helper(self, element):
+    def __sorting_helper(self, element) -> float:
         return self.scout.distance_to(element)
 
 # TODO
